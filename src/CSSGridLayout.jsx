61c075d78779f1a667713d1282b3299e7ff61f6b
Jordan Riser <riser.jordan@gmail.com>
2022-02-23 14:56:36 -0500

    adds ability to pass in properties to watch to trigger an animation

--- src/CSSGridLayout.jsx
+++ src/CSSGridLayout.jsx
@@ -18,84 +18,87 @@ export const defaultLayout = 'default'
 
 const GridContext = createContext({})
 
-const CSSGridLayout = forwardRef(({ children, layout }, ref) => {
-  // holds state to trigger a reRender of GridElements
-  const [reRenderState, setReRenderState] = useState(0)
+const CSSGridLayout = forwardRef(
+  ({ children, layout, watchedProps = [] }, ref) => {
+    // holds state to trigger a reRender of GridElements
+    const [reRenderState, setReRenderState] = useState(0)
 
-  const [shouldAnimate, setShouldAnimate] = useState(false)
-  const [layoutState, setLayoutState] = useState(defaultLayout)
+    const [shouldAnimate, setShouldAnimate] = useState(false)
+    const [layoutState, setLayoutState] = useState(defaultLayout)
 
-  useEffect(() => {
-    if (layout) {
-      setShouldAnimate(true)
-      setTimeout(() => {
-        setLayoutState(layout)
-        setReRenderState(Math.random())
-        setTimeout(() => {
-          setShouldAnimate(false)
-        }, 415)
-      }, 40)
-    }
-  }, [layout])
-
-  const handleResize = useCallback(() => {
-    setReRenderState(Math.random())
-  }, [])
-
-  useImperativeHandle(ref, () => ({
-    setLayout: (layout) => {
+    useEffect(() => {
       if (layout) {
-        setLayoutState(layout)
-      } else {
-        // reset layout to defaultLayout
-        setLayoutState(defaultLayout)
+        setShouldAnimate(true)
+        setTimeout(() => {
+          setLayoutState(layout)
+          setReRenderState(Math.random())
+          setTimeout(() => {
+            setShouldAnimate(false)
+          }, 415)
+        }, 40)
       }
-    }
-  }))
+    }, [layout, ...watchedProps])
 
-  const wrappedElements = useMemo(() => {
-    return Children.map(children, (child) => {
-      if (isValidElement(child)) {
-        return cloneElement(child, {
-          ...child.props,
-          animate: shouldAnimate,
-          reRender: reRenderState
-        })
+    const handleResize = useCallback(() => {
+      setReRenderState(Math.random())
+    }, [])
+
+    useImperativeHandle(ref, () => ({
+      setLayout: (layout) => {
+        if (layout) {
+          setLayoutState(layout)
+        } else {
+          // reset layout to defaultLayout
+          setLayoutState(defaultLayout)
+        }
       }
-      return child
-    })
-  }, [children, shouldAnimate, reRenderState])
+    }))
 
-  const gridElements = useMemo(() => {
-    return Children.map(children, (child) => {
-      if (isValidElement(child)) {
-        return createElement('div', {
-          className: `rcgl-grid-item ${child.props.name} ${
-            child?.props?.className || ''
-          }`
-        })
-      }
-      return child
-    })
-  }, [children])
+    const wrappedElements = useMemo(() => {
+      return Children.map(children, (child) => {
+        if (isValidElement(child)) {
+          return cloneElement(child, {
+            ...child.props,
+            animate: shouldAnimate,
+            reRender: reRenderState
+          })
+        }
+        return child
+      })
+    }, [children, shouldAnimate, reRenderState])
 
-  return (
-    <GridContext.Provider value={{}}>
-      <AutoSizer onResize={handleResize}>
-        {({ height, width }) => (
-          <div style={{ height, width }}>
-            <div className='rcgl-float-box'>{wrappedElements}</div>
-            <div className={`rcgl-grid ${layoutState}`}>{gridElements}</div>
-          </div>
-        )}
-      </AutoSizer>
-    </GridContext.Provider>
-  )
-})
+    const gridElements = useMemo(() => {
+      return Children.map(children, (child) => {
+        if (isValidElement(child)) {
+          return createElement('div', {
+            className: `rcgl-grid-item ${child.props.name} ${
+              child?.props?.className || ''
+            }`
+          })
+        }
+        return child
+      })
+    }, [children])
+
+    return (
+      <GridContext.Provider value={{}}>
+        <AutoSizer onResize={handleResize}>
+          {({ height, width }) => (
+            <div style={{ height, width }}>
+              <div className='rcgl-float-box'>{wrappedElements}</div>
+              <div className={`rcgl-grid ${layoutState}`}>{gridElements}</div>
+            </div>
+          )}
+        </AutoSizer>
+      </GridContext.Provider>
+    )
+  }
+)
 
 CSSGridLayout.propTypes = {
   children: PropTypes.node,
-  layout: PropTypes.string
+  layout: PropTypes.string,
+  watchedProps: PropTypes.array
 }
 
 export default CSSGridLayout
